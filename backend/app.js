932fbb6431925e72c27907d38be778528cca5baf
brijesh <brijeshamrutiya2612@gmail.com>
2022-07-28 17:33:09 +0530

    fifth_Testing_Commit

--- backend/app.js
+++ backend/app.js
@@ -2,29 +2,36 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
-import cors from "cors"
+import cors from "cors";
 import prodRouter from "./routes/products-routes";
 import productRouter from "./routes/userProducts-routes";
-import path from 'path';
-import dotenv from 'dotenv'
+import path from "path";
+import dotenv from "dotenv";
 import orderRouter from "./routes/orderRoutes.js";
 import SellerRoute from "./routes/SellerRoute";
 import ratingRouter from "./routes/RatingRoute";
 import sellerOrderRouter from "./routes/SellerOrderRoute";
-import bodyParser from 'body-parser'
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.urlencoded({extended:true}));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log(`Database is Connected! Listening to localhost 5000`);
+  })
+  .catch((err) => console.log(err));
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000"} )),
+  
+  const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/keys/paypal', (req, res)=>{
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
-})
+app.use(cors({ credentials: true, origin: "http://localhost:3000" })),
+  app.get("/api/keys/paypal", (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+  });
 
 app.use(cookieParser());
 app.use(express.json());
@@ -33,27 +40,19 @@ app.use("/api/products", prodRouter);
 app.use("/api/seller", SellerRoute);
 app.use("/api/orders", orderRouter);
 app.use("/api/sellerorders", sellerOrderRouter);
-app.use("/api/rating",  ratingRouter);
-
+app.use("/api/rating", ratingRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/Client/build')));
-app.get('*', (req, res)=>
-  res.sendFile(path.join(__dirname, '/Client/build/index.html'))
-)
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
-app.use((err, req, res, next)=>{
-  res.status(500).send({mesage: err.message})
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ mesage: err.message });
+});
 
-
-
-mongoose
-  .connect(
-    process.env.MONGODB_URI
-  )
-  .then(() => {
-    app.listen(5000);
-    console.log("Database is Connected! Listening to localhost 5000");
-  })
-  .catch((err) => console.log(err));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
