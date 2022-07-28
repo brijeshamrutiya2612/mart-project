16d2f241db5220d99107346b2aeb0f55edaf8e7b
brijesh <brijeshamrutiya2612@gmail.com>
2022-07-28 18:25:58 +0530

    BD

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
-import cors from "cors"
+import cors from "cors";
 import prodRouter from "./routes/products-routes";
 import productRouter from "./routes/userProducts-routes";
 import path from "path";
@@ -14,7 +14,6 @@ import sellerOrderRouter from "./routes/SellerOrderRoute";
 import bodyParser from "body-parser";
 
 dotenv.config();
-app.use(cors()),
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -23,15 +22,14 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-  
-  const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+app.use(cors()), app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-  app.get("/api/keys/paypal", (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-  });
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
 app.use(cookieParser());
 app.use(express.json());
@@ -43,16 +41,16 @@ app.use("/api/sellerorders", sellerOrderRouter);
 app.use("/api/rating", ratingRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
   res.status(500).send({ mesage: err.message });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
