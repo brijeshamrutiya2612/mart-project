6d7f3fdfadb8a3d7c367aa7554daafd782ae5dbf
brijesh <brijeshamrutiya2612@gmail.com>
2022-07-30 13:12:07 +0530

    app

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors({ credentials: true, origin: "https://shopping-mart-react-app.herokuapp.com/"} )),
+app.use(cors()),
+
+//{ credentials: true, origin: "https://localhost:3000"} )), app.use(cors({ credentials: true, origin: "https://shopping-mart-react-app.herokuapp.com/"} 
 
 app.get('/api/keys/paypal', (req, res)=>{
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
