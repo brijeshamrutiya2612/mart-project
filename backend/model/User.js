fefc9372225425d2b229ac1eb0493b56bb7b7e8f
brijesh <brijeshamrutiya2612@gmail.com>
2022-08-05 15:55:46 +0530

    Images

--- backend/model/User.js
+++ backend/model/User.js
@@ -8,63 +8,63 @@ const UserSchema = Schema(
       type: String,
       required: true,
     },
-    // lastname: {
-    //   type: String,
-    //   required: true,
-    // },
-    // email: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    //   validate(value) {
-    //     if (!validator.isEmail(value)) {
-    //       throw new Error("Email is Invalid");
-    //     }
-    //   },
-    // },
-    // password: {
-    //   type: String,
-    //   required: true,
-    //   select:false
-    // },
-    // address1: {
-    //   type: String,
-    //   required: true,
-    // },
-    // address2: {
-    //   type: String,
-    //   required: true,
-    // },
-    // address3: {
-    //   type: String,
-    //   required: true,
-    // },
-    // phone: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    //   minlength: 10,
-    //   maxlength: 10,
-    // },
-    // age: {
-    //   type: Number,
-    //   required: true,
-    //   max: 100,
-    // },
-    // avatar: {
-    //   public_id: {
-    //     type: String,
-    //     required: true,
-    //   },
-    //   url: {
-    //     type: String,
-    //     required: true,
-    //   },
-    // },
-    // role: {
-    //   type: Boolean,
-    //   default: "user",
-    // },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is Invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select:false
+    },
+    address1: {
+      type: String,
+      required: true,
+    },
+    address2: {
+      type: String,
+      required: true,
+    },
+    address3: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 10,
+      maxlength: 10,
+    },
+    age: {
+      type: Number,
+      required: true,
+      max: 100,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    role: {
+      type: Boolean,
+      default: "user",
+    },
   },
   { timestamps: true }
 );
