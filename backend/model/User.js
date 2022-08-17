6254a1e5c79969b53fbc4f1e1550e24fa52cded0
brijesh <brijeshamrutiya2612@gmail.com>
2022-08-17 16:11:47 +0530

    asas

--- backend/model/User.js
+++ backend/model/User.js
@@ -70,8 +70,8 @@ userSchema.methods.getJWTToken = function () {
 
 // Compare Password
 
-userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compareSync(enteredPassword, this.password);
 };
 
 // Generating Password Reset Token
